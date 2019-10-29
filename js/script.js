32bdac285ee67f615d1f593e75d5bc194fbb6f85
Maja <rojasmaja5@gmail.com>
2019-10-28 20:59:25 -0700

    trying to add pictures to my html/scss

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
+//listen to the select menu to change(whatching value)
+//send a request to the NYT API for data based on the value of the select menu
 
+//.append the data (abstract, multimedia[4].url, url) into the empty div in your HTML(.articles)
 
+$(document).ready(function() {
+  $("#select").on("change", function() {
+    const section = $(this).val();
+    console.log(section);
 
-
+    $.ajax({
+      method: "GET",
+      url:
+        "https://api.nytimes.com/svc/topstories/v2/" +
+        section +
+        ".json?api-key=4rt1RlE8LASv1wwdEoifJb3PweJksFgk",
+      dataType: "json"
+    }).done(function(data) {
+      for (let i = 0; i < 12; i++) {
+        console.log(data.results[i].title);
+        let p = document.createElement("p");
+        p.innerHTML = data.results[i].title;
+        // $(".articles").append(p);
+      }
+    });
+  });
+});
