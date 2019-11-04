ca12f2e1f08e7ed0b926fa73f8ef5e04a2e6ae6d
Maja <rojasmaja5@gmail.com>
2019-11-03 21:34:05 -0800

    cleaning the code from coments and getting it ready for submision

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   $(document).ready(function() {});
 
   $("#select").on("change", function() {
-    // $("header").css("height", "50vh");
     $("header").addClass("scale_header");
     if ($(this).val() === "sections") {
       $(".spin").hide();
@@ -15,18 +14,15 @@ document.addEventListener("DOMContentLoaded", function() {
       .toLowerCase();
     console.log($(this).val());
 
-    //console.log(data);
-
     $.ajax({
-      method: "GET", // gets the api json file from nyt
+      method: "GET",
       url:
         "https://api.nytimes.com/svc/topstories/v2/" +
         section +
-        ".json?api-key=4rt1RlE8LASv1wwdEoifJb3PweJksFgk", //api pth to the json file
+        ".json?api-key=4rt1RlE8LASv1wwdEoifJb3PweJksFgk",
       dataType: "json"
     })
       .done(function(data) {
-        //once above ajax is done, pass the data to this 'done' function
         const results = data.results
           .filter(function(items) {
             if (items.multimedia[4] !== undefined) {
@@ -37,20 +33,14 @@ document.addEventListener("DOMContentLoaded", function() {
           })
           .slice(0, 12);
 
-        //const results = data.results.slice(0, 12); //get 12 items from my data array
-
-        $("#articles").empty(); //clear out old articles
+        $("#articles").empty();
 
         $.each(results, function(index, value) {
-          //for each 12 'results' find key and value
-          //console.log(value);
-          //make variable for each value needed
           const link = value.url;
           const img = value.multimedia[4].url;
           const abstract = value.abstract;
           console.log(img);
 
-          //append a template block to html target container
           $("#articles").append(
             `<li class='storys' style='background-image:url(${img});'>
               <a class = 'nysite' href= ${link}>  
@@ -67,6 +57,6 @@ document.addEventListener("DOMContentLoaded", function() {
         $("#articles").append(
           "<p class='note'>Please choose a section on the button above.</p>"
         );
-      }); //close fail function!
-  }); //close event listner
+      });
+  });
 });
